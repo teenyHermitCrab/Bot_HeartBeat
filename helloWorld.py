2e898e69486b94f28e4816cfc88ec4cc32213f17
CorkHorde <teenyhermitcrab@gmail.com>
2024-10-07 11:41:48 -0700

    heartrate beats faster at closer distances. adding comments. code cleanup.
    This is still a basic script.  Needs refactor into classes

--- helloWorld.py
+++ helloWorld.py
@@ -5,6 +5,9 @@ import neopixel
 
 pixels = neopixel.NeoPixel(pin=board.NEOPIXEL, n=1, brightness=0.05)
 
+
+# TODO: make this helloWorld an investigation of colorwheel, or add a new file for that
+
 while True:
     pixels.fill((255, 0, 0))
     time.sleep(0.55)
